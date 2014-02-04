7dbb13981c6bb1c09e09949b5858da25ba820671
dev-xiligroup <contact@xiligroup.com>
2014-02-04 22:17:04 +0100

    2013-xili : fixes sidebar-2 options

--- twentythirteen-xili/sidebar.php
+++ twentythirteen-xili/sidebar.php
@@ -8,14 +8,14 @@
  * @subpackage Twenty_Thirteen_xili
  * @since Twenty Thirteen 1.0
  */
-if ( class_exists('xili_language') ) { // if temporary disabled 
-	
+if ( class_exists('xili_language') ) { // if temporary disabled
+
 	$options = get_theme_xili_options();
-	$curlang_suffix = ( the_curlang() == 'en_us' || the_curlang() == "" ) ? '' : '_'.the_curlang()  ; 
+	$curlang_suffix = ( the_curlang() == 'en_us' || the_curlang() == "" ) ? '' : '_'.the_curlang()  ;
 } else {
 	$curlang_suffix = '';
 }
-if ( $curlang_suffix != '' && !isset( $options['sidebar_'.'sidebar-1'] ) ) $curlang_suffix = '' ; //display default  - no clone
+if ( $curlang_suffix != '' && !isset( $options['sidebar_'.'sidebar-2'] ) ) $curlang_suffix = '' ; //display default  - no clone
 
 if ( is_active_sidebar( 'sidebar-2'  . $curlang_suffix  ) ) : ?>
 	<div id="tertiary" class="sidebar-container" role="complementary">
